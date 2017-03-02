e8de38f743db580e946061149bce46e99855f941
Kishor <kishor.07557@gmail.com>
2017-03-02 22:21:09 +0530

    minor changes

--- README.md
+++ README.md
@@ -5,5 +5,9 @@
 2. Add tags while creating the post {DONE}
 3. Login page design {DONE}
 4. Blog show page with the comments {DONE}
-5. Admin comments section
-6. Blog like/dislike section
+5. Ckeditor with codesnippet {DONE}
+6. Admin comments section
+7. Blog like/dislike section
+8. Facebook like/follow integration
+9. Scroll to top on blogs and show page {DONE}
+10. Go back to index from show page {DONE}
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,21 @@
 //= require jquery_ujs
 //= require ckeditor/init
 //= require_tree ./code
+//= require moment
+//= require bootstrap-datetimepicker
+
+$(document).ready(function() {
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 600) {
+            $(".scrollToTop").fadeIn(1000)
+        } else {
+            $(".scrollToTop").fadeOut(1000);
+        }
+    });
+
+    //Click event to scroll to top
+    $(".scrollToTop").click(function(){
+        $('html, body').animate({scrollTop : 0},500);
+        return false;
+    });
+});
